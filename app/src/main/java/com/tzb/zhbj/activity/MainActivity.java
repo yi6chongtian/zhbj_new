b560595750355de57de471ef23a70c492c334e05
tzb <tzb@yfyun.net>
2020-04-15 21:31:23 +0800

    闪屏页实现

--- app/src/main/java/com/tzb/zhbj/activity/MainActivity.java
+++ app/src/main/java/com/tzb/zhbj/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.tzb.zhbj;
+package com.tzb.zhbj.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.tzb.zhbj.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/tzb/zhbj/activity/SplashActivity.java
@@ -0,0 +1,65 @@
+package com.tzb.zhbj.activity;
+
+
+import android.animation.StateListAnimator;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.tzb.zhbj.R;
+
+public class SplashActivity extends Activity {
+
+    private ImageView mIv_horse;
+    private RelativeLayout mRl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mRl = findViewById(R.id.rl_root);
+        mIv_horse = findViewById(R.id.iv_horse);
+        //动画 旋转 缩放 渐变
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setDuration(1000);
+        rotateAnimation.setFillAfter(true);
+
+        ScaleAnimation scaleAnimation = new ScaleAnimation(.0f, 1.0f, .0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(1000);
+        scaleAnimation.setFillAfter(true);
+
+        AlphaAnimation alphaAnimation = new AlphaAnimation(.0f, 1.0f);
+        alphaAnimation.setDuration(2000);
+        alphaAnimation.setFillAfter(true);
+
+        AnimationSet animationSet = new AnimationSet(true);
+        animationSet.addAnimation(rotateAnimation);
+        animationSet.addAnimation(scaleAnimation);
+        animationSet.addAnimation(alphaAnimation);
+        mRl.startAnimation(animationSet);
+        animationSet.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+    }
+}
